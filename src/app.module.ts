0cf88f09ecc9fbba3c774dd78a0d0d8344691078
vunguyen2711 <vunguyen.forwork1995@gmail.com>
2025-12-24 22:27:15 +0700

    NS1-Init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TicketModule } from './ticket/ticket.module';
+import { TicketModule } from './ticket/ticket.module';
+
+@Module({
+  imports: [TicketModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
